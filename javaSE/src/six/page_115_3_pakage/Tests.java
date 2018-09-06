0c59d17379f567b7351c32ee0dc7637f621b7172
2390438889 <2390438889@qq.com>
2018-09-06 15:56:30 +0800

    测试

---
+++ javaSE/src/six/page_115_3_pakage/Tests.java
@@ -0,0 +1,13 @@
+package six.page_115_3_pakage;
+
+import org.junit.Test;
+
+import six.page_115_3_pakage.debugoff.Debug;
+import static util.alias.Print.*;
+public class Tests {
+	@Test
+	public void testDebug(){
+		Debug.debug();
+		Debug.debug();
+	}
+}

a0b99e83609986a2e79664f02591a9e1010ba9d7
2390438889 <2390438889@qq.com>
2018-09-07 19:29:42 +0800

    11

--- javaSE/src/test/lianxi.java
+++ javaSE/src/test/lianxi.java
@@ -3,12 +3,24 @@ import java.io.File;
 
 import org.junit.Test;
 
+import util.TranslateUtil;
+
 import static util.FileUtil.*;
 public class lianxi {
-	@Test
+	/*@Test
 	public void copyFileTest(){
 		try {
 			copyFile(new File("C:\\Users\\Administrator\\Desktop\\就业感想.txt"),new File("C:\\Users\\Administrator\\Desktop\\就业感想.doc"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}*/
+	//中文翻译成英文
+	@Test
+	public void translate() {
+		try {
+			System.out.println(TranslateUtil.getTranslateResult("就业"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
